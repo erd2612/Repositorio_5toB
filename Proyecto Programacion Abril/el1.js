71b2b078c88d4d2a04a38857f19c45a75c8eda0e
Unknown <unknown@example.com>
2020-04-24 18:01:02 -1200

    Aqui estan los ejercicios de la segunda practica
    Anteriormente había creado un nuevo repositorio, pero ahora lo subí a este mismo

---
+++ Proyecto Programacion Abril/el1.js
@@ -0,0 +1,18 @@
+
+var uno=prompt("Insertar un primer valor");
+var dos=prompt("Insertar un segundo valor");
+var restar;
+
+var restar= uno%dos;
+function estaeslafuncion(restar){
+
+if(restar==0){
+    return ("si es un numero multiplicativo");
+    }
+    else{
+        return ("no es un numero multiplicativo");
+    }
+}
+alert(estaeslafuncion(restar))
+
+
---
+++ Proyecto Programacion Abril/el2.js
@@ -0,0 +1,15 @@
+
+var k= prompt("Ingrese un numero");
+
+function isEaven(k){
+  for(var k=1; e>=2; e--){
+    k=k-2
+  }
+      if(k==1){
+        return ("true");
+      }
+      else if(k==1){
+        return ("false");
+      }
+    }
+ alert(isEaven(k))
